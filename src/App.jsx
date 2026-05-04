a169f0c1361e067fd6bf8ec9d4c5e989731bd6a3
Vimal Chamoli <vimaldchamoli@gmail.com>
2026-05-04 18:12:22 +0530

    fix protected routes and auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* 🔥 PROTECTED */}
       <Route
         path="/cart"
         element={
